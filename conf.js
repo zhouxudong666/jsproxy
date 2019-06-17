e058f2122fee9d57b8f3ceb4532149ce3a6874a5
zhouxudong666 <39668020+zhouxudong666@users.noreply.github.com>
2019-06-18 00:55:27 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -30,7 +30,7 @@ jsproxy_config({
       lines: [
         // 实验中...
         // 参考 https://github.com/EtherDream/jsproxy/tree/master/cf-worker
-        'node-cfworker.etherdream.com:8443'
+        'https://small-sun-1852.zxd.workers.dev:8443'
       ]
     }
   },
